e765f8dce61d4ecfe3d5885b03f50d9113bd1eea
RileyPenguin07 <149598289+RileyPenguin07@users.noreply.github.com>
2023-11-14 20:55:09 +0000

    ytfduyrfkyfg[p

---
+++ pringting art/pringting art 2.java
@@ -0,0 +1,25 @@
+Class pringtingart2.java
+{
+    System.out.print ("welcome");
+    {
+        System.out.print ("*");
+        {
+            System.out.print ("**");
+            {
+               System.out.print ("***");
+               {
+                System.out.print ("****");
+                {
+                 System.out.print ("***");
+                 {
+                    System.out.print ("**");
+                    {
+                        System.out.print ("*");
+                    }
+                 }
+                }
+               }
+            }
+        }
+    }
+}
