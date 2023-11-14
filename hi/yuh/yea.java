5ab46d67098b5b59951f8378f07887d0ef51aea3
RileyPenguin07 <149598289+RileyPenguin07@users.noreply.github.com>
2023-11-14 16:44:35 +0000

    kfks

---
+++ hi/yuh/yea.java
@@ -0,0 +1,15 @@
+{
+    Public static void main (Strings args[])
+    {
+        system.out.print ("Welcome printer");
+        {
+            system.out.print ("Riley Havins");
+            {
+                system.out.print ("Age 15")
+                {
+                    system.out.print ("goodbye");
+                }
+            }
+        }
+    }
+}
