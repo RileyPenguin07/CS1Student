15b04c945a184364253f9e190f1129ce70a3baa4
RileyPenguin07 <149598289+RileyPenguin07@users.noreply.github.com>
2023-11-28 21:22:37 +0000

    yes

---
+++ maddyLibs/madLibs.java
@@ -0,0 +1,30 @@
+class madLibs
+{
+    public static void main (String args[])
+    {
+        //Define 10 variables that correspond with the blanks in your madLibs
+        String noun1 = "bunny";
+
+        String noun2="frog";
+
+        String noun3 = "taylor";
+
+        String noun4 = "walmart";
+
+        String adjective1 = "weird";
+
+        String adjective2 = "quiet";
+
+        String adjective3 = "smart";
+
+        String verb1 = "sitting";
+
+        String verb2 = "running";
+
+        String verb3 = "floating";
+
+
+        //Write your story with concatenated variables
+        System.out.print("There was once a" + noun1 + "and he loved" + adjective1 + noun2 + "while it was" + verb1 "He started to" + verb2 + "and" + verb3 + "When he had a" + adjective2 + noun3 "He realized he liked" + adjective3 + noun4 "more");
+    }
+}
