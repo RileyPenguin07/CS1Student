1a531aef8d26907abc13a5824c2ce7ea20d0257e
RileyPenguin07 <149598289+RileyPenguin07@users.noreply.github.com>
2023-11-28 14:46:51 +0000

    ?

---
+++ pringtingArt/pringtingArt2.java
@@ -0,0 +1,21 @@
+class pringtingart
+{
+    Public Static Void Main (Strings args[])
+    {
+    System.out.println ("welcome");
+    
+    System.out.println ("*");
+    
+    System.out.println ("**");
+            
+    System.out.println ("***");
+               
+    System.out.println ("****");
+                
+    System.out.println ("***");
+                 
+    System.out.println ("**");
+                    
+    System.out.println ("*"); 
+    }
+}
