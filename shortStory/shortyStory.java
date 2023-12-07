0af048e1a21d635bbfc94e0b77878f87520592f3
RileyPenguin07 <149598289+RileyPenguin07@users.noreply.github.com>
2023-12-07 15:42:49 +0000

    kjhb

--- shortStory/shortyStory.java
+++ shortStory/shortyStory.java
@@ -1,4 +1,12 @@
 class shortyStory
 {
     public static void main (String args[])
+
+    {
+        System.out.println ("There once was a pony who loved cats");
+        System.out.println ("The ponys name was Charles");
+        System.out.println ("Charles loved his cat named Wiskey");
+        System.out.println ("Wiskey loves to play with a ball of yarn");
+        System.out.println ("Charles and Wiskey love each other very much");
+    }
 }
