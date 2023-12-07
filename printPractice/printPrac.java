489c3d587f0d9512a94646371ab7a2efb8690a16
RileyPenguin07 <149598289+RileyPenguin07@users.noreply.github.com>
2023-12-07 15:52:18 +0000

    hii

---
+++ printPractice/printPrac.java
@@ -0,0 +1,12 @@
+class printPrac
+{
+    public static void main (String args[])
+    {
+        System.out.println("*");
+        Sysyem.out.println("* *");
+        Sysyem.out.println("* * *");
+        Sysyem.out.println("* * * *");
+        Sysyem.out.println("* * * * *");
+        Sysyem.out.println("* * * * * *");
+    }
+}
