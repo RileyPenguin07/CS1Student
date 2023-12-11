ffbea11b92d2e2e7c8d22ccf71255cdc1091fde8
RileyPenguin07 <149598289+RileyPenguin07@users.noreply.github.com>
2023-12-11 19:42:13 +0000

    uv

---
+++ miniProblems/probFour.java
@@ -0,0 +1,13 @@
+class probFour
+{
+public static void main (String args[])
+
+{
+    int num1 = 2;
+    int num2 = 3;
+    int num3 = 4;
+    int num4 = 5;
+    
+    System.out.println(num1 + num2 + num3 + num4);
+}
+}
